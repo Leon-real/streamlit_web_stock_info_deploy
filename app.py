75ca8d82f7b105454b53341fe731713789d27f61
Leon <tutmr999@naver.com>
2023-09-21 15:00:47 +0900

    streamlit파일 추가

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# 페이지 기본 설정
+st.set_page_config(
+    page_icon='📈',
+    page_title='주식 정보',
+    layout='wide',
+)
+
+# 페이지 헤더, 서브헤더 제목 설정
+st.header("주식 정보 홈페이지에 오신걸 환영합니다.")
+st.subheader("맛 보기")
+
+#
---
+++ pages/1📄_Docs.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# 페이지 기본 설정
+st.set_page_config(
+    page_icon='📈',
+    page_title='주식 정보',
+    layout='wide',
+)
+
+# 페이지 헤더, 서브헤더 제목 설정
+st.header("홈페이지 문서")
+st.subheader("사용 방법")
+
+#
