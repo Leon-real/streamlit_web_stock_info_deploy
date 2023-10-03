1df4db6c5fc92e40802532acbaf996aec14e8f71
Leon <tutmr999@gmail.com>
2023-10-03 17:18:56 +0900

    코드 수정

--- app.py
+++ app.py
@@ -53,9 +53,7 @@ with st.spinner("정보 조회중 . . ."):
         container.dataframe(df[i:i+50])
     
     st.dataframe(data=df, use_container_width=True)
-    st.dataframe(df)
-    st.write(df)
-    st.table(df)
+    
     # # 페이지 번호를 선택할 수 있는 컨트롤 추가
     # page_number = st.slider('페이지 번호', 1, len(df) // 10 + 1, 1)
 
